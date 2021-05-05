6ebfaf93be1cb5720e891e0298ab2f5f57ca76a9
Marek Jaczewski <jaczewski147@gmail.com>
2021-05-05 22:52:10 +0200

    add card overlaping

--- src/app/components/face-down-cards/face-down-cards.component.ts
+++ src/app/components/face-down-cards/face-down-cards.component.ts
@@ -1,11 +1,4 @@
-import {
-    Component,
-    ElementRef,
-    HostBinding,
-    Input,
-    OnInit
-} from '@angular/core';
-import { DomSanitizer, SafeStyle } from '@angular/platform-browser';
+import { Component, Input, OnInit } from '@angular/core';
 import { Card } from 'src/app/models/table';
 
 @Component({
@@ -14,11 +7,26 @@ import { Card } from 'src/app/models/table';
     styleUrls: ['./face-down-cards.component.less']
 })
 export class FaceDownCardsComponent implements OnInit {
-    handOfCards: Card[];
+    readonly CARD_HEIGHT: number = 210;
+    readonly CARD_WIDTH: number = 140;
+    readonly CARD_OFFSET: number = 40;
+    marginX: string;
 
-    ngOnInit(): void {}
+    handOfCards: Card[];
 
     @Input() set cards(cards: Card[]) {
         this.handOfCards = cards;
+    }
+
+    @Input() cardOffset: number;
+
+    ngOnInit(): void {
+        this.setMarginX();
+    }
+
+    setMarginX(): void {
+        const offset = this.cardOffset ?? this.CARD_OFFSET;
+        const marginValue = (this.CARD_WIDTH - offset) / 2;
+        this.marginX = `-${marginValue}px`;
     }
 }
